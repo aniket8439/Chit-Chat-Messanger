2558554233619ac655195e3452063f500831033c
Aniket Sharma <aniket8439@gmail.com>
2022-12-29 15:48:47 +0530

    index.js changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,25 @@
 import React from "react";
 
-import { Router, Switch, Route } from "react-router-dom"
-import { createBrowserHistory } from 'history';
+import {  Switch, Route, BrowserRouter } from "react-router-dom"
+
 import { AuthProvider } from "../contexts/AuthContext"
 import Chats from "./Chats";
 import Login from "./Login";
 
 
 function App() {
-  const history = createBrowserHistory();
+ 
   return (
     <div style={{ fontFamily: 'Avenir' }}>
  
-      <Router history={history}>
+      <BrowserRouter>
          <AuthProvider> 
           <Switch>
-            <Route  exact path="/chats" component={Chats} />
-            <Route  exact path="/"  component={Login} /> 
+            <Route path="/chats" exact component = { Chats } />
+            <Route path="/"  exact component = { Login } /> 
           </Switch>
         </AuthProvider>
-      </Router>
+      </BrowserRouter>
     </div>
   )
 }
